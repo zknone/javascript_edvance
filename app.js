0ebf374773c2decf24c7c3f77c703279f9730f95
Streltsov Ivan <principdereva@gmail.com>
2024-01-06 14:39:42 +0100

    made first homework

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const data = [
+    {
+        id: 1,
+        name: 'Вася',
+    },
+    {
+        id: 2,
+        name: 'Петр',
+    },
+    {
+        id: 1,
+        name: 'Вася',
+    },
+    {
+        id: 1,
+        name: 'Вася',
+    },
+    {
+        id: 3,
+        name: 'Эмануил',
+    },
+    {
+        id: 4,
+        name: 'Веспуччи',
+    },
+];
+
+const makeUnique = (array) => {
+    let modifiedArray = [];
+    array.map(item => {
+        if (!modifiedArray.find(({ id }) => id === item.id)) {
+            modifiedArray.push(item);
+        }
+    });
+    return modifiedArray;
+}
+
+console.log(makeUnique(data));
