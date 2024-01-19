c2c8c9b66828dc6a6ccd1aca1f18e07eeac109a1
Streltsov Ivan <principdereva@gmail.com>
2024-01-19 16:58:56 +0100

    feat: function for age verification

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const userBirthDay = '2006/1/30';
+
+const checkBirthday = (date) => {
+    const birthDay = new Date(date);
+    const now = new Date();
+    const AGE_OF_MAJORITY = 18;
+    
+    const age = now.getFullYear() - birthDay.getFullYear();
+
+    if (age < AGE_OF_MAJORITY) return false;
+    if (age === AGE_OF_MAJORITY) {
+        if (now.getMonth() < birthDay.getMonth()) return false;
+        if ((now.getMonth() === birthDay.getMonth()) && (now.getDay() > birthDay.getDay())) return true;
+    }
+    return false;
+}
+
+console.log(checkBirthday(userBirthDay));
