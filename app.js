efaaa3fbf1325ba38e74d839bab7f6432e2f23fb
Streltsov Ivan <principdereva@gmail.com>
2024-01-26 17:22:25 +0100

    fixes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const userBirthDay = '2005/1/12';
+const userBirthDay = '2007/1/1';
 
 const checkBirthday = (date) => {
-    if (!date.split('/')[2]) return false;
+    if (Number(date.split('/')[2]) <= 0 || Number(date.split('/')[2]) >= 32 || !Number(date.split('/')[2])) return 'Вы ввели некорректный день';
+    if (Number(date.split('/')[1]) <= 0 || Number(date.split('/')[1]) >= 13 || !Number(date.split('/')[1])) return 'Вы ввели некорректный месяц';
 
     const birthDay = new Date(date);
     const now = new Date();
@@ -11,11 +12,11 @@ const checkBirthday = (date) => {
     
     const age = now.getFullYear() - birthDay.getFullYear();
 
-    if (age < AGE_OF_MAJORITY) return false;
+    if (age < AGE_OF_MAJORITY) return 'Нужно дорасти немного';
     if (age === AGE_OF_MAJORITY) {
-        if (now.getMonth() < birthDay.getMonth()) return false;
-        if ((now.getMonth() === birthDay.getMonth()) && (now.getDate() > birthDay.getDate())) return true;
+        if (now.getMonth() < birthDay.getMonth()) return 'Нужно дорасти немного';
+        if ((now.getMonth() === birthDay.getMonth()) && (now.getDate() < birthDay.getDate())) return 'Нужно дорасти немного';
     }
-    return true;
+    return 'Ооо. Вы большой мальчик';
 }
 console.log(checkBirthday(userBirthDay));
