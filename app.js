33aa10bf61f6c449cc7be758157f49fb7e52a61a
Streltsov Ivan <principdereva@gmail.com>
2024-02-19 16:22:55 +0100

    feat: refactir

--- app.js
+++ app.js
@@ -2,22 +2,30 @@
 
 const address = 'https://pokeapi.co/api/v2/pokemon/ditto';
 
-const request = new XMLHttpRequest();
-request.open('GET', address);
-request.send();
-request.addEventListener('load', function() {
-    const {abilities} = JSON.parse(this.responseText);
-    console.log(abilities[0].ability);
+async function fetchData(address, errorText) {
+    try {
+        const response = await fetch(address);
 
+        if (!response.ok) {
+            console.log(errorText)
+        };
+
+        return await response.json();
+
+    } catch (error) {
+        console.log(errorText)
+    }
+}
+
+fetchData(address, 'Something went wrong').then((data) => {
+    const { abilities } = data;
     const abilityUrl = abilities[0].ability.url;
 
-    const abilityRequest = new XMLHttpRequest();
-    abilityRequest.open('GET', abilityUrl);
-    abilityRequest.send();
-    abilityRequest.addEventListener('load', function() {
-        const {flavor_text_entries: textEntries} = JSON.parse(this.responseText);
+
+    fetchData(abilityUrl, 'Can not fetch ability').then((data) => {
+        const {flavor_text_entries: textEntries} = data;
+
         const {flavor_text: text} = textEntries.find(item => item.language.name === 'en');
         console.log(text);
-    });
-
-});
+    })
+})
