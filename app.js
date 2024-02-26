018749b0a7a9810af1f594269f5ff72e27586685
Streltsov Ivan <principdereva@gmail.com>
2024-02-26 22:17:28 +0100

    feat: getPosition

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const getPosition = () => {
+    return new Promise((resolve, reject)=> {
+        navigator.geolocation.getCurrentPosition((position) => {
+            if (!position.coords) {
+                reject (new Error('cant check'))
+            };
+
+            resolve (position.coords);
+        }
+    )}
+)}
+
+getPosition().then((data) => console.log(data)).catch((error) => console.log(error));
