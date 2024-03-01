61ede97f2c7caf3dc2e8cdd183c98024c0c4b344
Streltsov Ivan <principdereva@gmail.com>
2024-03-01 10:56:15 +0100

    fix: added fix

--- app.js
+++ app.js
@@ -16,19 +16,39 @@ const fetchData = async () => {
  const renderCard = (title, id) => {
     const item = document.createElement('li');
     item.id = id;
-    item.innerHTML = title;
+    const description = document.createElement('div');
+    const button = document.createElement('button');
+    description.innerHTML = title;
+    button.className = 'delete-button';
+    button.innerHTML = 'X';
+    button.setAttribute('data-id', id);
+    item.append(description);
+    item.append(button);
     return item;
+ }
+
+ const deleteHandler = (event) => {
+    if (event.target.className === 'delete-button') {
+        const listItemToDelete = event.target.parentNode;
+        listItemToDelete.remove();
+    }
  }
 
  const app = async () => {
     const { products } = await fetchData();
     const appContainer = document.querySelector('#app');
+    appContainer.addEventListener('click', deleteHandler);
+    const appButton = document.querySelector('#start-button');
 
     const list = document.createElement('ul');
     list.id = 'list';
     list.className = 'products-list';
 
-    products.map(item => list.append(renderCard(item.title, item.id)));
+    appButton.addEventListener('click', () => {
+        list.innerHTML='';
+        products.map(item => list.append(renderCard(item.title, item.id)));
+    })
+
     appContainer.append(list);
  };
 
