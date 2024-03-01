a1933f1f669454eefe81412d32ae7bdf3f1d9da8
Streltsov Ivan <principdereva@gmail.com>
2024-03-01 11:05:17 +0100

    fix

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, 'one');
+  });
+  
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'two');
+  });
+
 const race = (array) => Promise.race(array).then((value) => console.log(value)).catch((error) => console.log('Rejected :', error));
 
 async function app() {
